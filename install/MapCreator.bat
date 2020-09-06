61ee88a1f599892568f77a3822ba2ae5bbefc7fb
Aaron McGrath <aaron.mcgrath@adaptris.com>
2020-09-06 17:31:28 +0100

    Added a sample player attack animation.

--- install/MapCreator.bat
+++ install/MapCreator.bat
@@ -4,7 +4,7 @@ set CLASSPATH=.
 set HOME=.
 set JAVA_HOME=java\zulu-11\bin
 
-set CLASSPATH=%CLASSPATH%;%HOME%\lib\*;%HOME%\config;%HOME%\config\zones;%HOME%\config\images;%HOME%\config\images\Player\walk-right-armed;%HOME%\config\images\Player\walk-left-armed;%HOME%\config\maps;%HOME%\config\sounds;%HOME%\config\images\GoblinRun
+set CLASSPATH=%CLASSPATH%;%HOME%\lib\*;%HOME%\config;%HOME%\config\zones;%HOME%\config\images;%HOME%\config\images\Player\walk-right-armed;%HOME%\config\images\Player\walk-left-armed;%HOME%\config\maps;%HOME%\config\sounds;%HOME%\config\images\GoblinRun;%HOME%\config\images\Player\attack
 
 
 %JAVA_HOME%\java -cp %CLASSPATH% -Xmx2048m -Xdebug -Dlog4j.skipJansi=false -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=1045 amc.levelcreator.MapCreatorMain
